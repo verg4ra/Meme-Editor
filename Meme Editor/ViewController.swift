8b28faf885d7dc2008c720758018a00d5d399630
Edwin Vergara <heydot.at@gmail.com>
2016-03-18 01:15:48 -0500

    Display image picker when the Album button is pressed

--- Meme Editor/ViewController.swift
+++ Meme Editor/ViewController.swift
@@ -10,16 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func displayImagePicker() {
+        let pickerController = UIImagePickerController()
+        self.presentViewController(pickerController, animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
